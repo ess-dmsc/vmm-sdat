bb4f426de383204d93861fe24621ae2188c19ac1
Dorothea Pfeiffer <dorothea.pfeiffer@esss.se>
2022-10-31 15:54:12 +0100

    Update storePcapngData.py

--- monitoring/storePcapngData.py
+++ monitoring/storePcapngData.py
@@ -3,29 +3,26 @@ import os
 import subprocess
 import re
 import sys
-import uproot3 as uproot
-import pandas as pd
-import matplotlib.pyplot as plt
 import time as t
 
 #Edit these fields to determine the acquisition
-numberOfFiles = 3
-fileName = "run_xray"
-interface="en0"
+numberOfFiles = 100
+fileName = "sipm_test"
+interface="enp0s31f6"
 #Choose either duration in seconds or filesize in kB
-duration=6
-filesize=100
+duration=60
+filesize=1000000
 
 
 
 cnt = 0
 try:
-	while cnt < numberOfFiles:
-		theDate = t.strftime("%Y%m%d%H%M%S") 	
-		name = fileName + "_" + f'{cnt:05}' + "_" + str(theDate) + ".pcapng"
-		args = ['dumpcap', '-w', name, '-a', 'duration:' + str(duration), '-i', interface']
-		#args = ['dumpcap', '-w', name, '-a', 'filesize:' + str(filesize), '-i', interface]
-		subprocess.call(args)
-		cnt = cnt + 1
+    while cnt < numberOfFiles:
+        theDate = t.strftime("%Y%m%d%H%M%S") 	
+        name = fileName + "_" + f'{cnt:05}' + "_" + str(theDate) + ".pcapng"
+        args = ['dumpcap', '-w', name, '-a', 'duration:' + str(duration), '-i', interface]
+        #args = ['dumpcap', '-w', name, '-a', 'filesize:' + str(filesize), '-i', interface]
+        subprocess.call(args)
+        cnt = cnt + 1
 except OSError:
     pass
